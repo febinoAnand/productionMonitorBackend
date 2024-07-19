01a6d984f4a584eba58726cd2399039deb861a55
Anandkumar <febinosolutions@gmail.com>
2024-07-19 16:51:29 +0530

    added the mqtt ready app to migration in the server

--- mqttConnection/apps.py
+++ mqttConnection/apps.py
@@ -4,6 +4,6 @@ class MqttConnectionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mqttConnection'
 
-    # def ready(self):
-    #     import mqttConnection.mqtt as mqtt
-    #     mqtt.start_mqtt_client()
+    def ready(self):
+        import mqttConnection.mqtt as mqtt
+        mqtt.start_mqtt_client()
