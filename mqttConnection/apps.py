992026d0fb3fd1f3686fcbb88ff5ca5adb8de549
anandkumar <febinosolutions@gmail.com>
2024-08-05 22:01:45 +0530

    uncomment the start_mqtt_client in the mqttconnection/app.py file

--- mqttConnection/apps.py
+++ mqttConnection/apps.py
@@ -4,9 +4,9 @@ class MqttConnectionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mqttConnection'
 
-    # def ready(self):
-    #     import mqttConnection.mqtt as mqtt
+    def ready(self):
+        import mqttConnection.mqtt as mqtt
         
-    #     mqtt.start_mqtt_client()
+        mqtt.start_mqtt_client()
 
         
