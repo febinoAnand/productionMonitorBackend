2703cde597ff0f92419b25acbdfaded5b18613db
karthik <karthik202m@gmail.com>
2024-08-24 13:18:38 +0530

    Corrected designation field in Userauth

--- Userauth/models.py
+++ Userauth/models.py
@@ -14,14 +14,14 @@ class UnauthUser(models.Model):
     session_id = models.UUIDField(unique=True, null=False, blank=False)
     device_id = models.CharField(max_length=50, null=False, blank=False)
     otp_called = models.IntegerField(default=0, null=False, blank=False)
-    designation = models.CharField(max_length=15, null=True, blank=True)
+    designation = models.CharField(max_length=50, null=True, blank=True)
     is_existing_user = models.BooleanField(default=False, null=False, blank=False)
     verification_token = models.UUIDField(unique=True, null=True, blank=True)
     otp_wrong_count = models.IntegerField(default=0, null=False, blank=False)
 
 class UserDetail(models.Model):
     extUser = models.OneToOneField(User, on_delete=models.CASCADE, null=False, blank=False)
-    designation = models.CharField(max_length=15, null=True, blank=False)
+    designation = models.CharField(max_length=50, null=True, blank=False)
     mobile_no = models.CharField(unique=True, max_length=15, null=False, blank=False)
     employee_id = models.CharField(max_length=50, unique=True, null=True, blank=True)
     registered_status = models.BooleanField(default=False, null=False, blank=False)
